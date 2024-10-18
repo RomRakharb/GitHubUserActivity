61c269ffbb381b96855fbad27ac071e58772d165
Rom Rakharb <rom.rakharb@mailfence.com>
2024-10-18 19:47:55 +0700

    setup nix

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,8 @@ use reqwest::header::{ACCEPT, AUTHORIZATION};
 use std::env;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let args: Vec<String> = env::args().collect();
-    let username = args.get(1).unwrap();
+    let mut args = env::args();
+    let username = args.nth(1).unwrap();
 
     let body = reqwest::blocking::get(format!("https://api.github.com/users/{}/events", username))?
         .text()?;
