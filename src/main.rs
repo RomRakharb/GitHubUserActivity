1764ba8dc74ebee308ddb62125fd152be69d1a97
Rom Rakharb <rom.rakharb@mailfence.com>
2024-10-19 20:56:35 +0700

    can connect to github

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,33 @@
-use reqwest::header::{ACCEPT, AUTHORIZATION};
+use reqwest::header::{HeaderMap, ACCEPT, AUTHORIZATION, USER_AGENT};
+use serde_json::Value;
 use std::env;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut args = env::args();
     let username = args.nth(1).unwrap();
+    let token = "";
 
-    let body = reqwest::blocking::get(format!("https://api.github.com/users/{}/events", username))?
-        .text()?;
+    let mut headers = HeaderMap::new();
+    headers.insert(ACCEPT, "application/vnd.github+json".parse()?);
+    headers.insert(AUTHORIZATION, format!("Bearer {}", token).parse()?);
+    headers.insert("X-GitHub-Api-Version", "2022-11-28".parse()?);
+    headers.insert(USER_AGENT, "GitHub ROM".parse()?);
 
-    println!("body = {body:?}");
+    let client = reqwest::blocking::Client::new();
+    let response = client
+        .get(format!("https://api.github.com/users/{}/events", username))
+        .headers(headers)
+        .send()?;
+
+    // Check if the response was successful
+    if response.status().is_success() {
+        let body: Value = response.json()?;
+        let pretty_body = serde_json::to_string_pretty(&body)?;
+        println!("{}", pretty_body);
+    } else {
+        println!("Error: {}", response.status());
+        let error_body = response.text()?;
+        println!("Response body: {}", error_body);
+    }
     Ok(())
 }
