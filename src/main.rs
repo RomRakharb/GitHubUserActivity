12ae10e17d95e1633684da8d7eb3972adadd536f
Rom Rakharb <rom.rakharb@mailfence.com>
2024-10-19 21:20:39 +0700

    get token from file

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,29 @@
 use reqwest::header::{HeaderMap, ACCEPT, AUTHORIZATION, USER_AGENT};
 use serde_json::Value;
 use std::env;
+use std::fs::File;
+use std::io::Read;
+
+enum Action {
+    Get(String),
+    SetToken(String),
+}
+
+fn get_token() -> std::io::Result<String> {
+    let mut token = String::new();
+    let mut file = File::open("token")?;
+    file.read_to_string(&mut token)?;
+    Ok(token)
+}
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut args = env::args();
     let username = args.nth(1).unwrap();
-    let token = "";
+    let token = get_token()?;
 
     let mut headers = HeaderMap::new();
     headers.insert(ACCEPT, "application/vnd.github+json".parse()?);
-    headers.insert(AUTHORIZATION, format!("Bearer {}", token).parse()?);
+    headers.insert(AUTHORIZATION, format!("Bearer {token}").parse()?);
     headers.insert("X-GitHub-Api-Version", "2022-11-28".parse()?);
     headers.insert(USER_AGENT, "GitHub ROM".parse()?);
 
