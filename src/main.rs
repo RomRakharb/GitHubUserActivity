fc350b78679a57d4fac05b77d62a2301e79a3e5d
Rom Rakharb <rom.rakharb@mailfence.com>
2024-10-20 10:13:17 +0700

    fix header invalid

--- src/main.rs
+++ src/main.rs
@@ -19,25 +19,28 @@ fn get_token() -> std::io::Result<String> {
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut args = env::args();
     let username = args.nth(1).unwrap();
-    let token = get_token()?;
+    let binding = get_token()?;
+    let token = binding.trim_end_matches('\n');
 
     let mut headers = HeaderMap::new();
     headers.insert(ACCEPT, "application/vnd.github+json".parse()?);
     headers.insert(AUTHORIZATION, format!("Bearer {token}").parse()?);
     headers.insert("X-GitHub-Api-Version", "2022-11-28".parse()?);
-    headers.insert(USER_AGENT, "GitHub ROM".parse()?);
+    headers.insert(USER_AGENT, "GitHub User Activity".parse()?);
 
     let client = reqwest::blocking::Client::new();
+
     let response = client
         .get(format!("https://api.github.com/users/{}/events", username))
         .headers(headers)
         .send()?;
 
-    // Check if the response was successful
     if response.status().is_success() {
-        let body: Value = response.json()?;
-        let pretty_body = serde_json::to_string_pretty(&body)?;
-        println!("{}", pretty_body);
+        let body: Value = serde_json::from_str(response.text().unwrap().as_str())?;
+        if let Some(each) = body.as_array() {
+            println!("{:?}", each);
+        }
+        // println!("{:?}", body);
     } else {
         println!("Error: {}", response.status());
         let error_body = response.text()?;
